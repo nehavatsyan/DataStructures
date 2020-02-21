4a234da52d9c77cef03a3a44e5630464bb461f7d
Neha <nehavatsyan49207@gmail.com>
2020-02-21 12:31:21 +0530

    added linked list

--- ds/linkedlist/linked.go
+++ ds/linkedlist/linked.go
@@ -2,9 +2,41 @@ package linkedlist
 
 import "fmt"
 
-type list struct {
+type node struct {
+	data interface{}
+	next *node
 }
 
-func main() {
-	fmt.Printf("hello")
+type listOps interface {
+	createList()
+	insertNode()
+	deletedNode()
+	findElement()
+}
+
+func (start node) createList() *node {
+	start.data = 0
+	start.next = nil
+	return &start
+}
+
+func (start *node) insertNode(n int) {
+	var newNode *node
+	newNode.data = n
+	newNode.next = nil
+	start = newNode
+	fmt.Printf("--data inserted--")
+
+}
+func (start *node) deletedNode(a int) {
+	var n, p *node
+	n = start
+	for n.next != nil {
+		p.next = n
+		if n.data == a {
+			temp := n.next
+			p.next = temp
+		}
+		n = n.next
+	}
 }
